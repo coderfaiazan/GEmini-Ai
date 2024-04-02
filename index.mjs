13029e91bf24c5d0a6991ed31a20a5c02ac580b1
coderfaiazan <faizan678siddiqui@gmail.com>
2024-04-02 18:16:27 +0530

    convert to json

--- index.mjs
+++ index.mjs
@@ -14,14 +14,20 @@ const input = comment +' '+ pdfPath;
 
 async function run() {
    try {
-     const model = genAI.getGenerativeModel({ model: "gemini-pro"});
+     const model = genAI.getGenerativeModel({ 
+      model: "gemini-pro",
+      response_format:{"type":"json_object"},
+     
+    });
      const prompt = `${input}.make qiuzz of 10 mcq having 4 options and answer seperatly in foolowing json
      format.{"question":[{"id":0,"question":"","options":[],"answer":""}, ...]} `
      
      const result = await model.generateContent(prompt)
      const response = await result.response;
-     const text = response.text();
-     console.log(text);
+     //console.log("Content:", response.candidates[0].content);
+     const json = JSON.stringify(response.candidates[0].content)
+     const jsonparse = JSON.parse(json)
+     console.log(jsonparse);
    } catch (error) {
        console.log("Can't generate Because of the internal error",error);
    }
